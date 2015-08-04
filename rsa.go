5d3c816b7e609a6af79523cef7240be754b0df9c
Liang Xiang <liangxiang@creditease.cn>
2015-08-04 18:15:57 +0800

    support kissy rsa

--- rsa.go
+++ rsa.go
@@ -4,7 +4,10 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/hex"
 	"encoding/pem"
+	"errors"
+	"math/big"
 )
 
 func GenerateRSAKey() (*rsa.PrivateKey, error) {
@@ -38,4 +41,71 @@ func PrivateKeyString(key *rsa.PrivateKey) []byte {
 			Type:  "RSA PRIVATE KEY",
 			Bytes: x509.MarshalPKCS1PrivateKey(key),
 		})
+}
+
+func pkcs1pad2(s []byte, n int) (*big.Int, error) {
+	if n < len(s)+11 {
+		return nil, rsa.ErrMessageTooLong
+	}
+	ba := make([]byte, n)
+	i := len(s) - 1
+	for i >= 0 && n > 0 {
+		c := s[i]
+		i -= 1
+		if c < 128 {
+			n -= 1
+			ba[n] = c
+		} else if (c > 127) && (c < 255) {
+			n -= 1
+			ba[n] = (c & 63) | 128
+			n -= 1
+			ba[n] = (c >> 6) | 192
+		} else {
+			n -= 1
+			ba[n] = (c & 63) | 128
+			n -= 1
+			ba[n] = ((c >> 6) & 63) | 128
+			n -= 1
+			ba[n] = (c >> 12) | 224
+		}
+	}
+	n -= 1
+	ba[n] = 0
+	x := make([]byte, 1)
+	for n > 2 {
+		x[0] = byte(0)
+		for x[0] == 0 {
+			rand.Read(x)
+		}
+		n -= 1
+		ba[n] = x[0]
+	}
+	n -= 1
+	ba[n] = 2
+	n -= 1
+	ba[n] = 0
+	ret := new(big.Int)
+	return ret.SetBytes(ba), nil
+}
+
+func rsaDoPublic(x *big.Int, pub *rsa.PublicKey) *big.Int {
+	e := big.NewInt(int64(pub.E))
+	ret := new(big.Int)
+	return ret.Exp(x, e, pub.N)
+}
+
+func PKCS1Pad2Encrypt(s []byte, pub *rsa.PublicKey) (string, error) {
+	m, err := pkcs1pad2(s, int(pub.N.BitLen()+7)>>3)
+	if err != nil {
+		return "", err
+	}
+	c := rsaDoPublic(m, pub)
+	if c == nil {
+		return "", errors.New("do public failed")
+	}
+	h := hex.EncodeToString(c.Bytes())
+	if len(h)&1 == 0 {
+		return h, nil
+	}
+	return "0" + h, nil
 }
--- rsa_test.go
+++ rsa_test.go
@@ -1,6 +1,10 @@
 package caspercloud
 
 import (
+	"crypto/rsa"
+	"fmt"
+	"math/big"
+	"strconv"
 	"testing"
 )
 
@@ -11,4 +15,29 @@ func TestRSA(t *testing.T) {
 	}
 	t.Log(string(PrivateKeyString(pk)))
 	t.Log(string(PublicKeyString(&pk.PublicKey)))
+}
+
+func TestTaobaoRSA(t *testing.T) {
+	pbk := "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685"
+	n := new(big.Int)
+	_, err := fmt.Sscanf(pbk, "%x", n)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(n.String())
+	e, err := strconv.ParseInt("10001", 16, 64)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(e)
+	publicKey := &rsa.PublicKey{
+		N: n,
+		E: int(e),
+	}
+	password := []byte("xxxxxxxx")
+	out, err := PKCS1Pad2Encrypt(password, publicKey)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(out)
 }
